ac09c335b1dda40886ee738869ec9d5c62ada251
John Unland <unlandj2012@gmail.com>
2026-09-21 10:03:06 -0500

    Enhance bash and PowerShell profiles with additional aliases and environment variables; add Terraform configuration files for plugin caching.

--- dot_config/powershell/profile.ps1
+++ dot_config/powershell/profile.ps1
@@ -1,4 +1,9 @@
 # Managed by chezmoi
 
+# Aliases and functions for PowerShell
 Set-Alias ll Get-ChildItem
-function .. { Set-Location .. }
+
+# If sudo.exe is available, alias it to gsudo.
+if (Get-Command sudo.exe -ErrorAction SilentlyContinue) {
+    function sudo { gsudo @args }
+}
